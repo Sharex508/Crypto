0d36091901cff2558731518a59c80faef6bfb72e
Sharex508 <47905309+Sharex508@users.noreply.github.com>
2022-06-30 22:40:57 +0530

    update

--- wazirx-connector-python-master/Buy.py
+++ wazirx-connector-python-master/Buy.py
@@ -218,6 +218,7 @@ def task(db_resp, api_resp, data):
                 "timestamp": int(time.time() * 1000)}
         msg = data1
         notisend(msg)
+        update_coin_record(data)
 
 def coin_buy(data):
     try:
