7f2fe027dbfb6c13ef1cad39adfff14086877f7e
Sharex508 <47905309+Sharex508@users.noreply.github.com>
2022-07-21 14:35:27 +0530

    Update reset.py

--- wazirx-connector-python-master/reset.py
+++ wazirx-connector-python-master/reset.py
@@ -8,6 +8,26 @@ import json
 from wazirx_sapi_client.rest import Client
 from psycopg2 import Error
 
+def table_Delete_crypto ():
+    conn = psycopg2.connect(
+    database="crypto", user='postgres', password='harsha508', host='database-1.cigflazwbdyg.ap-south-1.rds.amazonaws.com', port= '5432'
+    )
+    conn.autocommit = True
+
+    #Creating a cursor object using the cursor() method
+    cursor = conn.cursor()
+
+    #Preparing query to create a database  
+    #sql = '''CREATE database crypto''';
+
+    #Creating a database
+    cursor.execute("DROP TABLE trading_test")
+    cursor.execute("DROP TABLE coin_buy")
+    print("Database deteted successfully........")
+
+    #Closing the connection
+    conn.close()
+
 
 def table_Create_crypto ():
 
